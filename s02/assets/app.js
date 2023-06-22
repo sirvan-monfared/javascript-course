bc8c9b81fa968174e7aaad12ca91c8d0a10b6751
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-06-22 14:43:36 +0430

    s02e16

--- s02/assets/app.js
+++ s02/assets/app.js
@@ -2,7 +2,60 @@ const defaultValue = 0;
 
 let result = defaultValue;
 
+function writeOutput(initialResult, inputValue, operand)
+{
+    const operationDesc = `${initialResult} ${operand} ${inputValue}`;
 
-function sum(a, b) {
-    return a + b;
+    writeResult(result);
+
+    writeOperation(operationDesc);
 }
+
+
+function sum() {
+
+    const initialResult = result;
+
+    const inputValue = +inputElm.value;
+
+    result = result + inputValue;
+    
+    writeOutput(initialResult, inputValue, '+');
+}
+
+function subtract() {
+    const initialResult = result;
+
+    const inputValue = +inputElm.value;
+
+    result = result - inputValue;
+    
+    writeOutput(initialResult, inputValue, '-');
+}
+
+function multiply() {
+    const initialResult = result;
+
+    const inputValue = +inputElm.value;
+
+    result = result * inputValue;
+    
+    writeOutput(initialResult, inputValue, '*');
+}
+
+
+function divide() {
+    const initialResult = result;
+
+    const inputValue = +inputElm.value;
+
+    result = result / inputValue;
+    
+    writeOutput(initialResult, inputValue, '/');
+}
+
+
+addElm.addEventListener('click', sum);
+minusElm.addEventListener('click', subtract);
+multiplyElm.addEventListener('click', multiply);
+divideElm.addEventListener('click', divide);
