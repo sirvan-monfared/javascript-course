05d3a213704ede9f8c0d15dbc2f91d08c15e8aaa
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-07-23 02:16:15 +0430

    s05e08

--- s05/app.js
+++ s05/app.js
@@ -5,13 +5,41 @@ const generateRandomNumber = function() {
    return Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
 }
 
+const getPlayerChoice = () => {
+  const choice = prompt(`Please choose a number between ${MIN_NUMBER} and ${MAX_NUMBER}`);
+
+  return parseInt(choice);
+}
+
 const startGame = () => {
+  let attempt = 0;
   const chosenNumber = generateRandomNumber();
 
-  console.log(chosenNumber);
+  let playerChoice;
+
+  while(playerChoice !== chosenNumber) {
+
+    playerChoice = getPlayerChoice();
+
+    if (isNaN(playerChoice) || playerChoice > MAX_NUMBER || playerChoice < MIN_NUMBER) {
+      alert("you entered an invalid number. please restart the game");
+
+      return;
+    }
+
+    attempt++;
+
+    if (playerChoice === chosenNumber) {
+      alert(`hooorah. you guessed the right number in ${attempt} attempts`);
+    } else if (playerChoice < chosenNumber) {
+      alert('too low. guess a higher number');
+    } else {
+      alert('too high. guess a lower number');
+    }
+  }
+  
 }
 
 
 const button = document.querySelector('#guess');
-
 button.addEventListener('click', startGame);
