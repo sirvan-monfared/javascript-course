247957ff3b448e74f8d4c30433f522f071ba0061
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-07-23 02:48:06 +0430

    s05e09

--- s05/app.js
+++ s05/app.js
@@ -11,17 +11,37 @@ const getPlayerChoice = () => {
   return parseInt(choice);
 }
 
+const isGameEnded = (playerChoice, chosenNumber) => {
+  return playerChoice === chosenNumber;
+}
+
+const checkIfPlayerWon = (playerChoice, chosenNumber, attempt) => {
+  if (isGameEnded(playerChoice, chosenNumber)) {
+    alert(`hooorah. you guessed the right number in ${attempt} attempts`);
+  } else if (playerChoice < chosenNumber) {
+    alert('too low. guess a higher number');
+  } else {
+    alert('too high. guess a lower number');
+  }
+}
+
+const isPlayerChoiceInvalid = (playerChoice) => {
+  return isNaN(playerChoice) || playerChoice > MAX_NUMBER || playerChoice < MIN_NUMBER;
+  // return ! isNaN(playerChoice) && playerChoice > MIN_NUMBER && playerChoice < MAX_NUMBER;
+}
+
+
 const startGame = () => {
   let attempt = 0;
   const chosenNumber = generateRandomNumber();
 
   let playerChoice;
 
-  while(playerChoice !== chosenNumber) {
-
+  while(! isGameEnded(playerChoice, chosenNumber)) {
     playerChoice = getPlayerChoice();
 
-    if (isNaN(playerChoice) || playerChoice > MAX_NUMBER || playerChoice < MIN_NUMBER) {
+    
+    if (isPlayerChoiceInvalid(playerChoice)) {
       alert("you entered an invalid number. please restart the game");
 
       return;
@@ -29,13 +49,7 @@ const startGame = () => {
 
     attempt++;
 
-    if (playerChoice === chosenNumber) {
-      alert(`hooorah. you guessed the right number in ${attempt} attempts`);
-    } else if (playerChoice < chosenNumber) {
-      alert('too low. guess a higher number');
-    } else {
-      alert('too high. guess a lower number');
-    }
+    checkIfPlayerWon(playerChoice, chosenNumber, attempt);
   }
   
 }
