b342d748eb96195015c2d1916501eca623b8eda4
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-07-30 19:06:12 +0430

    s06e04

---
+++ s06/app.js
@@ -0,0 +1,13 @@
+// const elements = document.getElementsByClassName('paragraph');
+
+// for (element of elements) {
+//     console.dir(element);
+// }
+
+// const elm = document.getElementsByTagName('li');
+// const elm2 = document.querySelectorAll('li');
+// console.log(elm);
+// console.log(elm2);
+
+const input = document.querySelector('.frm-input');
+console.log(input);
