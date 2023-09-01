2987ad28fb12eb204a5cd007a336199d2a183f2a
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-01 20:10:45 +0430

    s09e10 end of book store project

--- s09-lara-store-project/app.js
+++ s09-lara-store-project/app.js
@@ -1,17 +1,170 @@
+const listWrapper = document.getElementById("list-wrapper");
+const cartIcon = document.getElementById('cart-icon');
+const cartTotal = document.getElementById('cart-total');
+const cartNotificationElm = document.getElementById('cart-notification');
+const cartItemsListElm = document.getElementById('cart-items-list');
+const emptyCartElm = document.getElementById('empty-cart');
+const modalElm = document.getElementById('modal');
+
 const books = [];
+const cartItems = new Set([]);
+
+const viewButtonHandler = (id) => {
+
+    const foundBook = books.find(book => book.id === id);
+
+    const modalContentElm = document.getElementById('modal-content');
+
+    modalContentElm.querySelector('img').src = foundBook.imageURL;
+    modalContentElm.querySelector('h2').innerText = foundBook.title;
+    modalContentElm.querySelector('p').innerText = foundBook.author;
+    modalContentElm.querySelector('.book-summary').innerHTML = foundBook.summary;
+    modalContentElm.querySelector('.book-price').innerHTML = foundBook.showPrice();
+
+    modalContentElm.querySelector('button').addEventListener('click', addToCartHandler.bind(null, id));
+
+    modalElm.classList.remove('hidden');
+};
+
+const updateCartEmptyStatus = () => {
+    if (cartItems.size) {
+        cartItemsListElm.classList.remove('hidden');
+        emptyCartElm.classList.add('hidden');
+    } else {
+        cartItemsListElm.classList.add('hidden');
+        emptyCartElm.classList.remove('hidden');
+    }
+}
+
+const removeFromCarthandler = (id) => {
+
+    if (! cartItems.has(id)) return;
+
+    cartItems.delete(id);
+
+    document.getElementById(`cart-item-${id}`).remove();
+
+    updateCartTotalPrice();
+    updateCartNotification();
+    updateCartEmptyStatus();
+}
+
+const updateCartTotalPrice = () => {
+    const totalPrice = Array.from(cartItems).reduce((prevValue, book_id) => {
+        return prevValue + books.find(book => book.id === book_id).price
+    }, 0);
+
+    cartTotal.innerText = `$${totalPrice}`;
+}
+
+const updateCartNotification = () => {
+    cartNotificationElm.innerText = cartItems.size;
+    if(cartItems.size) {
+        cartNotificationElm.classList.remove('hidden');
+    } else {
+        cartNotificationElm.classList.add('hidden');
+    }
+}
+
+const addToCartHandler = (id) => {
+
+    console.log('called' + id);
+
+    if (cartItems.has(id)) {
+        return;
+    }
+
+    cartItems.add(id);
+
+    updateCartEmptyStatus();
+    
+    const foundBook = books.find((item) => item.id === id);
+
+    
+    const newCartElm = document.createElement('li');
+    newCartElm.className = 'flex items-center justify-between gap-4 truncate h-14  cursor-pointer hover:bg-gray-100';
+    newCartElm.id = `cart-item-${id}`;
+
+    newCartElm.innerHTML = `
+        <img class="w-14 h-14 rounded-md" src="${foundBook.imageURL}">
+        <div class="flex flex-col justify-between items-start h-full truncate">
+            <p class="truncate">${foundBook.title}</p>
+            <p class="font-semibold">$${foundBook.price}</p>
+        </div>
+        <button class="text-red-500 px-2 h-full cursor-pointer">x</button>
+    `;
+
+    newCartElm.addEventListener('click', removeFromCarthandler.bind(null, foundBook.id));
+
+    cartItemsListElm.firstElementChild.append(newCartElm);
+
+    updateCartTotalPrice();
+
+    updateCartNotification();
+};
+
+const generateHTMLCodeForBook = (book) => {
+    const newElm = document.createElement("div");
+    newElm.className = "bg-white p-4 shadow-md rounded-lg";
+    newElm.innerHTML = `
+            <div class="">
+                <img src="${book.imageURL}" alt="Book Title" class="w-full h-full object-cover mb-4">
+                <h3 class="text-xl font-semibold">${book.title}</h3>
+                <p class="text-gray-600">${book.author}</p>
+                <p class="text-blue-500 font-semibold mt-2 text-blue-500"><span>${book.showPrice()}</span></p>
+                <div class="flex items-center justify-between text-xs gap-2" id="book-footer-${book.id}">
+                </div>
+            </div>
+    `;
+
+    const bookFooter = newElm.querySelector(`#book-footer-${book.id}`);
+
+    const viewButtonElm = document.createElement("a");
+    viewButtonElm.className =
+        "mt-4 px-4 py-2 bg-gray-500 text-white hover:bg-gray-400 rounded-lg cursor-pointer";
+    viewButtonElm.textContent = "View Details";
+
+    const addToCartElm = viewButtonElm.cloneNode(true);
+    addToCartElm.textContent = "Add to Cart";
+    addToCartElm.classList.add("bg-blue-500", "hover:bg-blue-400");
+
+    bookFooter.append(viewButtonElm);
+    bookFooter.append(addToCartElm);
+
+    viewButtonElm.addEventListener(
+        "click",
+        viewButtonHandler.bind(null, book.id)
+    );
+
+    addToCartElm.addEventListener(
+        "click", 
+        addToCartHandler.bind(null, book.id)
+    );
+
+    listWrapper.append(newElm);
+};
 
 const addNewBook = (title, author, prevPrice, price, summary, imageURL) => {
-  const newBook = {
-    id: Math.ceil(new Date().getTime().toString() * Math.random()),
-    title,
-    author,
-    prevPrice,
-    price,
-    summary,
-    imageURL,
-  };
+    const newBook = {
+        id: Math.ceil(new Date().getTime().toString() * Math.random()),
+        title,
+        author,
+        prevPrice,
+        price,
+        summary,
+        imageURL,
+        showPrice() {
+            if (! prevPrice) {
+                return `<span class="text-blue-500">$${this.price}</span>`;
+            }
 
-  books.push(newBook);
+            return `<span class="line-through text-red-500">$${this.prevPrice}</span> <span class="text-blue-500">$${this.price}</span>`;
+        }
+    };
+
+    books.push(newBook);
+
+    generateHTMLCodeForBook(newBook);
 };
 
 const addDummyBooks = () => {
@@ -66,7 +219,7 @@ const addDummyBooks = () => {
         120,
         80,
         `Harry Potter and the Half-Blood Prince is a fantasy novel written by British author J. K. Rowling and the sixth and penultimate novel in the Harry Potter series. Set during Harry Potter's sixth year at Hogwarts, the novel explores the past of the boy wizard's nemesis, Lord Voldemort, and Harry's preparations for the final battle against Voldemort alongside his headmaster and mentor Albus Dumbledore.
-            The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic on 16 July 2005, as well as in several other countries. It sold almost seven million copies in the first 24 hours after its release,[1] a record eventually broken by its sequel, Harry Potter and the Deathly Hallows.[2] There were many controversies before and after it was published, including the right-to-read copies delivered before the release date in Canada. Reception to the novel was generally positive, and it won several awards and honours, including the 2006 British Book of the Year award.`,
+                The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic on 16 July 2005, as well as in several other countries. It sold almost seven million copies in the first 24 hours after its release,[1] a record eventually broken by its sequel, Harry Potter and the Deathly Hallows.[2] There were many controversies before and after it was published, including the right-to-read copies delivered before the release date in Canada. Reception to the novel was generally positive, and it won several awards and honours, including the 2006 British Book of the Year award.`,
         "https://m.media-amazon.com/images/I/81p2+4nYtkL._AC_UF894,1000_QL80_.jpg"
     );
 
@@ -76,13 +229,29 @@ const addDummyBooks = () => {
         null,
         80,
         `Harry Potter and the Deathly Hallows is a fantasy novel written by British author J. K. Rowling and the seventh and final novel in the Harry Potter series. It was released on 21 July 2007 in the United Kingdom by Bloomsbury Publishing, in the United States by Scholastic, and in Canada by Raincoast Books. The novel chronicles the events directly following Harry Potter and the Half-Blood Prince (2005) and the final confrontation between the wizards Harry Potter and Lord Voldemort.
-        
-            Deathly Hallows shattered sales records upon release, surpassing marks set by previous titles of the Harry Potter series. It holds the Guinness World Record for most novels sold within 24 hours of release, with 8.3 million sold in the US and 2.65 million in the UK.[1][2] Reception to the book was generally positive, and the book won the 2008 Colorado Blue Spruce Book Award, and the American Library Association named it the "Best Book for Young Adults". A play, Harry Potter and the Cursed Child, was written by Jack Thorne, and based on an original story written by J. K. Rowling, John Tiffany, and Thorne, premiering at the Palace Theatre in London on 30 July 2016. Rowling has referred to the play as "the eighth Harry Potter story", and is set nineteen years after the events of the Deathly Hallows, effectively picking up where the epilogue of the seventh book ended.
-            `,
+            
+                Deathly Hallows shattered sales records upon release, surpassing marks set by previous titles of the Harry Potter series. It holds the Guinness World Record for most novels sold within 24 hours of release, with 8.3 million sold in the US and 2.65 million in the UK.[1][2] Reception to the book was generally positive, and the book won the 2008 Colorado Blue Spruce Book Award, and the American Library Association named it the "Best Book for Young Adults". A play, Harry Potter and the Cursed Child, was written by Jack Thorne, and based on an original story written by J. K. Rowling, John Tiffany, and Thorne, premiering at the Palace Theatre in London on 30 July 2016. Rowling has referred to the play as "the eighth Harry Potter story", and is set nineteen years after the events of the Deathly Hallows, effectively picking up where the epilogue of the seventh book ended.
+                `,
         "https://m.media-amazon.com/images/I/81Whkax7IGL._AC_UF894,1000_QL80_.jpg"
     );
 };
 
 addDummyBooks();
 
-console.log(books);
+
+cartIcon.addEventListener('click', () => {
+    cartIcon.nextElementSibling.classList.toggle('hidden');
+})
+
+
+modalElm.querySelector('button').addEventListener('click', () => {
+    modalElm.classList.add('hidden');
+
+
+
+    const modalAddToCartElm = modalElm.querySelector('.modal-add-to-cart');
+
+    const newButton = modalAddToCartElm.cloneNode(true);
+
+    modalAddToCartElm.replaceWith(newButton);
+});
