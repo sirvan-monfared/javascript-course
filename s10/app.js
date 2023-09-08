346773f0603a20ec024532236f3b03f7f633c80e
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-08 17:29:07 +0430

    s10e06

--- s10/app.js
+++ s10/app.js
@@ -1,40 +1,18 @@
 const Person = function(name, lastName) {
-    // instance properties
     this.name = name;
     this.lastName = lastName;
-
-    // do not define methods like this in constructor function
-    // this.fullName = function() {
-    //     console.log(this.name + ' ' + this.lastName);
-    // }
 }
 
-const person1 = new Person('sirvan', 'monfared');
-const person2 = new Person('nimal', 'maleki');
-const person3 = new Person('maryam', 'tahani');
-// console.log(person1, person2, person3);
-
-// person1.fullName();
+const sirvan = new Person('sirvan', 'monfared');
 
 // 1. creates new {}
 // 2. function called, this points to newly created {}
-// 3. {} links to prototype
+// 3. {} links to prototype  __proto__
 // 4- returns automiatically the created {}
 
 Person.prototype.fullName = function() {
     console.log(this.name + ' ' + this.lastName);
 }
 
-console.log(Person.prototype);
-
-person1.fullName();
-
-console.dir(Person.prototype.isPrototypeOf(Person));
-
-console.log(Object.getPrototypeOf(person1));
-
-Person.prototype.race = 'Human';
-
-console.log(Person.prototype.hasOwnProperty('race'));
-
+sirvan.fullName();
 
