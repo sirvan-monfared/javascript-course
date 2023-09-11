8fffce303066c1d024665476fa4911d687e8168a
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-11 15:38:41 +0430

    s10e07

--- s10/app.js
+++ s10/app.js
@@ -1,18 +1,20 @@
-const Person = function(name, lastName) {
-    this.name = name;
-    this.lastName = lastName;
+const arr = [1, 10, 15, 17, 15, 10, 9, 13];
+
+// arr = new Array(1, 10, 15, 17)  Object
+
+Array.prototype.unique = function() {
+    return [...new Set(this)];
 }
 
-const sirvan = new Person('sirvan', 'monfared');
+console.log(arr.__proto__.__proto__.__proto__)
 
-// 1. creates new {}
-// 2. function called, this points to newly created {}
-// 3. {} links to prototype  __proto__
-// 4- returns automiatically the created {}
+console.log(arr.unique())
 
-Person.prototype.fullName = function() {
-    console.log(this.name + ' ' + this.lastName);
-}
+console.log([10,15,6,6,8,9,9,14].unique())
 
-sirvan.fullName();
+const obj = { name: 'sirvan'};
+// const obj = new Object()   obj.name = 'sirvn'  Object.prototype == obj.__proto__
 
+const h1 = document.querySelector('h1');
+
+console.dir(x => x + 1)
