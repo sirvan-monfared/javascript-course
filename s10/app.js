3df70d7582d35fa6fdf7b43d843c4cf36c9e351a
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-12 23:50:45 +0430

    s10e11

--- s10/app.js
+++ s10/app.js
@@ -32,39 +32,59 @@
 
 
 
+// class Person {
+//     constructor(name, lastName, age) {
+//         this.name = name;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+
+//     // instance methods
+//     fullName() {
+//         return `your name is ${this.name} ${this.lastName} and your age is ${this.age}`;
+//     }
+
+//     sayHello() {
+//         alert(this.fullName())
+//     }
+
+//     // static methods
+//     static goToParking() {
+//         console.log('heyyy');
+//     }
+// }
 
 
-class Person {
-    constructor(name, lastName, age) {
+
+// const sirvan = new Person('sirvan', 'monfared', 25);
+// console.log(sirvan);
+
+// console.log(sirvan.__proto__ === Person.prototype);
+
+// console.log(sirvan.fullName());
+
+// Person.goToParking()
+
+// sirvan.goToParking();
+
+const proto = {
+    init(name, lastName, age) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
-    }
+    },
 
-    // instance methods
     fullName() {
         return `your name is ${this.name} ${this.lastName} and your age is ${this.age}`;
     }
-
-    sayHello() {
-        alert(this.fullName())
-    }
-
-    // static methods
-    static goToParking() {
-        console.log('heyyy');
-    }
 }
 
+const sirvan = Object.create(proto);
+// sirvan.name = 'sirvan';
+// sirvan.lastName = 'monfared';
+// sirvan.age = 25;
+sirvan.init('sirvan', 'monfared', 25);
 
-
-const sirvan = new Person('sirvan', 'monfared', 25);
 console.log(sirvan);
-
-console.log(sirvan.__proto__ === Person.prototype);
-
-console.log(sirvan.fullName());
-
-Person.goToParking()
-
-sirvan.goToParking();
+console.log(sirvan.fullName())
+// console.log(sirvan.__proto__ === proto)
