4b8f651e7c25cfdd0ce922eec56b75d0e3e319f8
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-13 01:31:54 +0430

    s10e13

--- s10/app.js
+++ s10/app.js
@@ -1,22 +1,35 @@
 class User {
-    constructor(name, lastName, age, permission) {
+    constructor(name, lastName, age) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
-        this.permission = permission
+
+        if (this.constructor.name === 'User') {
+            this.login();
+        }
     }
 
     login() {
-        console.log(`${this.name} ${this.lastName} are logged in`);
+        console.log(`${this.name} ${this.lastName} is logged in`);
     }
 
     logout() {
-        console.log(`${this.name} ${this.lastName} are logged out`);
+        console.log(`${this.name} ${this.lastName} is logged out`);
     }
 }
 
 
 class Admin extends User {
+    constructor(name, lastName, age, permission) {
+        super(name, lastName, age);
+
+        this.permission = permission;
+        this.login();
+    }
+
+    login() {
+        console.log(`hey admin. welcome ${this.permission}`);
+    }
 
     deleteUser(user) {
         users = users.filter(u => {
@@ -28,17 +41,14 @@ class Admin extends User {
 }
 
 const sirvan = new User('sirvan', 'monfared', 25);
-const maryam = new User('maryam', 'tahani', 29);
-const ghazal = new User('ghazal', 'abbaskhanian', 28);
-// sirvan.deleteUser()
+console.log(sirvan.constructor.name);
+// const maryam = new User('maryam', 'tahani', 29);
+// const ghazal = new User('ghazal', 'abbaskhanian', 28);
 
 const jack = new Admin('jack', 'milano', 32, 'SuperAdmin');
+console.log(jack.constructor.name)
+// jack.login();
+// let users = [sirvan, maryam, ghazal, jack];
 
-let users = [sirvan, maryam, ghazal, jack];
-console.log(jack);
-
-// jack.deleteUser(sirvan);
-
-sirvan.delteUser(ghazal);
 
 
