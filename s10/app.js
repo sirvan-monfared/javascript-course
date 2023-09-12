717736aa095b051e608e009c6628f295d22f04a9
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-13 00:31:29 +0430

    s10e12

--- s10/app.js
+++ s10/app.js
@@ -1,90 +1,44 @@
-// const Car = function(model, speed) {
-//     this.model = model;
-//     this.speed = speed;
-// }
-
-// Car.prototype.accelerate = function() {
-//     this.speed += 15;
-//     console.log(`your ${this.model} speed is ${this.speed} km/h`);
-// }
-
-// Car.prototype.brake = function() {
-//     this.speed -= 10;
-//     console.log(`your ${this.model} speed is ${this.speed} km/h`);
-// }
-
-// Car.goToParking = function() {
-//     console.log('heyyy');
-// }
-
-// const dena =  new Car('Dena', 120);
-// const bmw = new Car('bmw', 150);
-
-// Car.goToParking()
-// dena.goToParking();
-
-// dena.accelerate();
-// dena.accelerate();
-// dena.accelerate();
-// dena.brake();
-// dena.accelerate();
-
-
-
-
-// class Person {
-//     constructor(name, lastName, age) {
-//         this.name = name;
-//         this.lastName = lastName;
-//         this.age = age;
-//     }
-
-//     // instance methods
-//     fullName() {
-//         return `your name is ${this.name} ${this.lastName} and your age is ${this.age}`;
-//     }
-
-//     sayHello() {
-//         alert(this.fullName())
-//     }
-
-//     // static methods
-//     static goToParking() {
-//         console.log('heyyy');
-//     }
-// }
-
-
-
-// const sirvan = new Person('sirvan', 'monfared', 25);
-// console.log(sirvan);
-
-// console.log(sirvan.__proto__ === Person.prototype);
-
-// console.log(sirvan.fullName());
-
-// Person.goToParking()
-
-// sirvan.goToParking();
-
-const proto = {
-    init(name, lastName, age) {
+class User {
+    constructor(name, lastName, age, permission) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
-    },
+        this.permission = permission
+    }
 
-    fullName() {
-        return `your name is ${this.name} ${this.lastName} and your age is ${this.age}`;
+    login() {
+        console.log(`${this.name} ${this.lastName} are logged in`);
+    }
+
+    logout() {
+        console.log(`${this.name} ${this.lastName} are logged out`);
     }
 }
 
-const sirvan = Object.create(proto);
-// sirvan.name = 'sirvan';
-// sirvan.lastName = 'monfared';
-// sirvan.age = 25;
-sirvan.init('sirvan', 'monfared', 25);
 
-console.log(sirvan);
-console.log(sirvan.fullName())
-// console.log(sirvan.__proto__ === proto)
+class Admin extends User {
+
+    deleteUser(user) {
+        users = users.filter(u => {
+            return u.name !== user.name
+        })
+
+        console.log(users);
+    }
+}
+
+const sirvan = new User('sirvan', 'monfared', 25);
+const maryam = new User('maryam', 'tahani', 29);
+const ghazal = new User('ghazal', 'abbaskhanian', 28);
+// sirvan.deleteUser()
+
+const jack = new Admin('jack', 'milano', 32, 'SuperAdmin');
+
+let users = [sirvan, maryam, ghazal, jack];
+console.log(jack);
+
+// jack.deleteUser(sirvan);
+
+sirvan.delteUser(ghazal);
+
+
