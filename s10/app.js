44e08dd3d930c8442b22eb7c4e05c92a49385334
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-15 00:37:16 +0430

    s10e14

--- s10/app.js
+++ s10/app.js
@@ -1,54 +1,73 @@
-class User {
-    constructor(name, lastName, age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
+// class User {
+//     constructor(name, lastName, age) {
+//         this.name = name;
+//         this.lastName = lastName;
+//         this.age = age;
 
-        if (this.constructor.name === 'User') {
-            this.login();
-        }
-    }
+//         if (this.constructor.name === 'User') {
+//             this.login();
+//         }
+//     }
 
-    login() {
-        console.log(`${this.name} ${this.lastName} is logged in`);
-    }
+//     login() {
+//         console.log(`${this.name} ${this.lastName} is logged in`);
+//     }
 
-    logout() {
-        console.log(`${this.name} ${this.lastName} is logged out`);
-    }
+//     logout() {
+//         console.log(`${this.name} ${this.lastName} is logged out`);
+//     }
+// }
+
+
+// class Admin extends User {
+//     constructor(name, lastName, age, permission) {
+//         super(name, lastName, age);
+
+//         this.permission = permission;
+//         this.login();
+//     }
+
+//     login() {
+//         console.log(`hey admin. welcome ${this.permission}`);
+//     }
+
+//     deleteUser() {
+//         console.log('deleting...')
+//     }
+// }
+
+// const sirvan = new User('sirvan', 'monfared', 25);
+
+// const jack = new Admin('jack', 'milano', 32, 'SuperAdmin');
+
+const User = function(name, lastName, age) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
 }
 
-
-class Admin extends User {
-    constructor(name, lastName, age, permission) {
-        super(name, lastName, age);
-
-        this.permission = permission;
-        this.login();
-    }
-
-    login() {
-        console.log(`hey admin. welcome ${this.permission}`);
-    }
-
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.name !== user.name
-        })
-
-        console.log(users);
-    }
+User.prototype.login = function() {
+    console.log(`${this.name} ${this.lastName} is logged in`);
 }
 
-const sirvan = new User('sirvan', 'monfared', 25);
-console.log(sirvan.constructor.name);
-// const maryam = new User('maryam', 'tahani', 29);
-// const ghazal = new User('ghazal', 'abbaskhanian', 28);
+const nima = new User('nima', 'maleki', 18);
 
-const jack = new Admin('jack', 'milano', 32, 'SuperAdmin');
-console.log(jack.constructor.name)
-// jack.login();
-// let users = [sirvan, maryam, ghazal, jack];
+const Admin = function(name, lastName, age, permission) {
+    User.call(this, name, lastName, age);
+    this.permission = permission;
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function() {
+    console.log(`${this.name} with ${this.permission} is deleting a user ...`); 
+}
+
+const sirvan = new Admin('sirvan', 'monfared', 25, 'Super Admin');
+sirvan.login();
 
 
+console.log(sirvan instanceof Admin);
+console.log(sirvan instanceof User);
+console.log(sirvan instanceof Object);
 
