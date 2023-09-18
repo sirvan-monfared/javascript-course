5da8efb5cbff950af24a857e97a749258c834890
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-18 22:40:54 +0430

    s11e04

--- s11-book-store-oop/app.js
+++ s11-book-store-oop/app.js
@@ -1,5 +1,5 @@
-const listUI = document.getElementById("list-wrapper");
-const cartIconElm = document.getElementById("cart-icon");
+
+
 const cartItemsListElm = document.getElementById('cart-items-list');
 const cartEmptyElm = document.getElementById('empty-cart');
 const cartTotalElm = document.getElementById('cart-total');
@@ -7,72 +7,20 @@ const cartNotification = document.getElementById('cart-notification');
 const modalElm = document.getElementById('modal');
 
 
-const books = [];
+
 const cartItems = new Set([]);
 
+{
+  id: 3456436436,
+  quantity: 3
+},
+{
+  id: 3456436436,
+  quantity: 2
+}
+
 const fillDummyBooks = () => {
-  addBook(
-    "Harry Potter and the Philosophers Stone",
-    "J. K. Rowling",
-    60,
-    null,
-    "The book is about 11 year old Harry Potter, who receives a letter saying that he is invited to attend Hogwarts, school of witchcraft and wizardry. He then learns that a powerful wizard and his minions are after the sorcerer's stone that will make this evil wizard immortal and undefeatable.",
-    "https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_568,c_scale/jackets/9781408855652.jpg"
-  );
-
-  addBook(
-    "Harry Potter and the Chamber of Secrets",
-    "J. K. Rowling",
-    55,
-    120,
-    'The plot follows Harrys second year at Hogwarts School of Witchcraft and Wizardry, during which a series of messages on the walls of the schools corridors warn that the "Chamber of Secrets" has been opened and that the "heir of Slytherin" would kill all pupils who do not come from all-magical families.',
-    "https://m.media-amazon.com/images/I/81S0LnPGGUL._AC_UF894,1000_QL80_.jpg"
-  );
-
-  addBook(
-    "Harry Potter and the Prisoner of Azkaban",
-    "J. K. Rowling",
-    65,
-    140,
-    'Harry Potter & The Prisoner of Azkaban is about Harry\'s 3rd year at Hogwarts. Along with friends Ron and Hermione, Harry investigates the case of Sirius Black, an escaped prisoner from Azkaban, the wizard prison.',
-    "https://m.media-amazon.com/images/I/81f7bXC-tTL._AC_UF894,1000_QL80_.jpg"
-  );
-
-  addBook(
-    "Harry Potter and the Goblet of Fire",
-    "J. K. Rowling",
-    70,
-    130,
-    `It follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry, and the mystery surrounding the entry of Harry's name into the Triwizard Tournament, in which he is forced to compete. The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic.`,
-    "https://m.media-amazon.com/images/I/810jKiNChxL._AC_UF894,1000_QL80_.jpg"
-  );
-  addBook(
-    "Harry Potter and the Order of the Phoenix",
-    "J. K. Rowling",
-    80,
-    null,
-    `Harry Potter and the Order of the Phoenix is a fantasy novel written by British author J. K. Rowling and the fifth novel in the Harry Potter series. It follows Harry Potter's struggles through his fifth year at Hogwarts School of Witchcraft and Wizardry, including the surreptitious return of the antagonist Lord Voldemort, O.W.L. exams, and an obstructive Ministry of Magic. The novel was published on 21 June 2003 by Bloomsbury in the United Kingdom, Scholastic in the United States, and Raincoast in Canada. It sold five million copies in the first 24 hours of publication`,
-    "https://m.media-amazon.com/images/I/81a4yXpXjnL._AC_UF1000,1000_QL80_.jpg"
-  );
-  addBook(
-    "Harry Potter and the Half-Blood Prince",
-    "J. K. Rowling",
-    80,
-    120,
-    `Harry Potter and the Half-Blood Prince is a fantasy novel written by British author J. K. Rowling and the sixth and penultimate novel in the Harry Potter series. Set during Harry Potter's sixth year at Hogwarts, the novel explores the past of the boy wizard's nemesis, Lord Voldemort, and Harry's preparations for the final battle against Voldemort alongside his headmaster and mentor Albus Dumbledore.
-    The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic on 16 July 2005, as well as in several other countries. It sold almost seven million copies in the first 24 hours after its release,[1] a record eventually broken by its sequel, Harry Potter and the Deathly Hallows.[2] There were many controversies before and after it was published, including the right-to-read copies delivered before the release date in Canada. Reception to the novel was generally positive, and it won several awards and honours, including the 2006 British Book of the Year award.`,
-    "https://m.media-amazon.com/images/I/81p2+4nYtkL._AC_UF894,1000_QL80_.jpg"
-  );
-  addBook(
-    "Harry Potter and the Deathly Hallows",
-    "J. K. Rowling",
-    80,
-    null,
-    `Harry Potter and the Deathly Hallows is a fantasy novel written by British author J. K. Rowling and the seventh and final novel in the Harry Potter series. It was released on 21 July 2007 in the United Kingdom by Bloomsbury Publishing, in the United States by Scholastic, and in Canada by Raincoast Books. The novel chronicles the events directly following Harry Potter and the Half-Blood Prince (2005) and the final confrontation between the wizards Harry Potter and Lord Voldemort.
-
-    Deathly Hallows shattered sales records upon release, surpassing marks set by previous titles of the Harry Potter series. It holds the Guinness World Record for most novels sold within 24 hours of release, with 8.3 million sold in the US and 2.65 million in the UK.[1][2] Reception to the book was generally positive, and the book won the 2008 Colorado Blue Spruce Book Award, and the American Library Association named it the "Best Book for Young Adults". A play, Harry Potter and the Cursed Child, was written by Jack Thorne, and based on an original story written by J. K. Rowling, John Tiffany, and Thorne, premiering at the Palace Theatre in London on 30 July 2016. Rowling has referred to the play as "the eighth Harry Potter story", and is set nineteen years after the events of the Deathly Hallows, effectively picking up where the epilogue of the seventh book ended.`,
-    "https://m.media-amazon.com/images/I/81Whkax7IGL._AC_UF894,1000_QL80_.jpg"
-  );
+  
 };
 
 const removeEmptyCartItem = () => {
@@ -176,67 +124,17 @@ const viewDetailsHandler = (id) => {
 };
 
 const generateBookHTML = (book) => {
-  const newElm = document.createElement("div");
-  newElm.className = "bg-white p-4 shadow-md rounded-lg";
-  newElm.innerHTML = `
-        <div class="">
-            <img src="${
-              book.imageUrl
-            }" alt="Book Title" class="w-full h-full object-cover mb-4">
-            <h3 class="text-xl font-semibold">${book.title}</h3>
-            <p class="text-gray-600">${book.author}</p>
-            <p class="text-blue-500 font-semibold mt-2 text-blue-500">${book.showPrice()}</p>
-            <div class="flex items-center justify-between text-xs gap-2" id="book-footer-${
-              book.id
-            }">
-            </div>
-        </div>
-    `;
 
-  const newBookFooter = newElm.querySelector(`#book-footer-${book.id}`);
 
-  const viewDetailsButton = document.createElement("a");
-  viewDetailsButton.className =
-    "mt-4 px-4 py-2 bg-gray-500 text-white hover:bg-gray-400 rounded-lg cursor-pointer";
-  viewDetailsButton.textContent = "View Details";
-  newBookFooter.append(viewDetailsButton);
+ 
 
-  const addToCartButton = viewDetailsButton.cloneNode(true);
-  addToCartButton.textContent = "Add to Cart";
-  addToCartButton.classList.add("bg-blue-500", "hover:bg-blue-400");
-  newBookFooter.append(addToCartButton);
-
-  viewDetailsButton.addEventListener(
-    "click",
-    viewDetailsHandler.bind(null, book.id)
-  );
-
-  addToCartButton.addEventListener(
-    "click",
-    addToCartHandler.bind(null, book.id)
-  );
+  
 
   listUI.append(newElm);
 };
 
 const addBook = (title, author, price, prevPrice, description, imageUrl) => {
-  const newBook = {
-    id: Math.ceil(new Date().getTime().toString() * Math.random()),
-    title,
-    author,
-    prevPrice,
-    price,
-    imageUrl,
-    description,
-    comments: [],
-    showPrice() {
-      if (!this.prevPrice) {
-        return `<span>$${this.price}</span>`;
-      }
-
-      return `<span class="line-through text-red-500">$${this.prevPrice}</span> <span>$${this.price}</span>`;
-    },
-  };
+ 
 
   books.push(newBook);
   generateBookHTML(newBook);
@@ -244,10 +142,8 @@ const addBook = (title, author, price, prevPrice, description, imageUrl) => {
 
 fillDummyBooks();
 
-const cartHandler = () => {
-  cartIconElm.nextElementSibling.classList.toggle("hidden");
-};
+
 
 modalElm.querySelector('#modal-close').addEventListener('click', hideModal)
 
-cartIconElm.addEventListener("click", cartHandler);
+
---
+++ s11-book-store-oop/oop.js
@@ -0,0 +1,180 @@
+class Book {
+    constructor(title, author, prevPrice, price, imageUrl, description) {
+        this.id = Math.ceil(new Date().getTime().toString() * Math.random())
+        this.title = title;
+        this.author = author;
+        this.prevPrice = prevPrice;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    showPrice() {
+        if (!this.prevPrice) {
+            return `<span>$${this.price}</span>`;
+        }
+
+        return `<span class="line-through text-red-500">$${this.prevPrice}</span> <span>$${this.price}</span>`;
+    }
+
+    render() {
+        const newElm = document.createElement("div");
+        newElm.className = "bg-white p-4 shadow-md rounded-lg";
+        newElm.innerHTML = `
+            <div class="">
+                <img src="${this.imageUrl}" alt="Book Title" class="w-full h-full object-cover mb-4">
+                <h3 class="text-xl font-semibold">${this.title}</h3>
+                <p class="text-gray-600">${this.author}</p>
+                <p class="text-blue-500 font-semibold mt-2 text-blue-500">${this.showPrice()}</p>
+                <div class="flex items-center justify-between text-xs gap-2" id="book-footer-${this.id}"></div>
+            </div>
+        `;
+
+        const newBookFooter = newElm.querySelector(`#book-footer-${this.id}`);
+
+        const viewDetailsButton = document.createElement("a");
+        viewDetailsButton.className =
+          "mt-4 px-4 py-2 bg-gray-500 text-white hover:bg-gray-400 rounded-lg cursor-pointer";
+        viewDetailsButton.textContent = "View Details";
+        newBookFooter.append(viewDetailsButton);
+      
+        const addToCartButton = viewDetailsButton.cloneNode(true);
+        addToCartButton.textContent = "Add to Cart";
+        addToCartButton.classList.add("bg-blue-500", "hover:bg-blue-400");
+        newBookFooter.append(addToCartButton);
+
+        viewDetailsButton.addEventListener(
+            "click",
+            this.viewDetailsHandler
+        );
+        
+        addToCartButton.addEventListener(
+            "click",
+            this.addToCartHandler.bind(this)
+        );
+
+        return newElm;
+    }
+
+    viewDetailsHandler() {
+        console.log('view details');
+    }
+
+    addToCartHandler() {
+        console.log(this);
+        new Cart().add(this.id);
+    }
+}
+
+
+class BookList {
+    constructor() {
+        this.books = [];
+        this.listUI = document.getElementById("list-wrapper");
+    }
+
+    add(title, author, prevPrice, price, description, imageUrl) {
+        const newBook = new Book(title, author, prevPrice, price, imageUrl, description);
+
+        this.books.push(newBook);
+
+        this.listUI.append(newBook.render());
+    }
+}
+
+class Cart {
+    constructor() {
+        this.items = [];
+    }
+
+    add(id) {
+        console.log(id);
+        this.items.push(id);
+        console.log(this.items);
+    }
+}
+
+class App {
+    constructor() {
+        this.bookList = new BookList();
+
+        this.handleUiInteractions();
+        this.generateDemoBooks();
+    }
+
+    handleUiInteractions() {
+        const cartIconElm = document.getElementById("cart-icon");
+
+        cartIconElm.addEventListener("click", () => {
+            cartIconElm.nextElementSibling.classList.toggle("hidden");
+        });
+    }
+
+    generateDemoBooks() {
+        this.bookList.add(
+            "Harry Potter and the Philosophers Stone",
+            "J. K. Rowling",
+            60,
+            null,
+            "The book is about 11 year old Harry Potter, who receives a letter saying that he is invited to attend Hogwarts, school of witchcraft and wizardry. He then learns that a powerful wizard and his minions are after the sorcerer's stone that will make this evil wizard immortal and undefeatable.",
+            "images/9781408855652.jpg"
+          );
+        
+          this.bookList.add(
+            "Harry Potter and the Chamber of Secrets",
+            "J. K. Rowling",
+            55,
+            120,
+            'The plot follows Harrys second year at Hogwarts School of Witchcraft and Wizardry, during which a series of messages on the walls of the schools corridors warn that the "Chamber of Secrets" has been opened and that the "heir of Slytherin" would kill all pupils who do not come from all-magical families.',
+            "images/81S0LnPGGUL._AC_UF894,1000_QL80_.jpg"
+          );
+        
+          this.bookList.add(
+            "Harry Potter and the Prisoner of Azkaban",
+            "J. K. Rowling",
+            65,
+            140,
+            'Harry Potter & The Prisoner of Azkaban is about Harry\'s 3rd year at Hogwarts. Along with friends Ron and Hermione, Harry investigates the case of Sirius Black, an escaped prisoner from Azkaban, the wizard prison.',
+            "images/81f7bXC-tTL._AC_UF894,1000_QL80_.jpg"
+          );
+        
+          this.bookList.add(
+            "Harry Potter and the Goblet of Fire",
+            "J. K. Rowling",
+            70,
+            130,
+            `It follows Harry Potter, a wizard in his fourth year at Hogwarts School of Witchcraft and Wizardry, and the mystery surrounding the entry of Harry's name into the Triwizard Tournament, in which he is forced to compete. The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic.`,
+            "images/810jKiNChxL._AC_UF894,1000_QL80_.jpg"
+          );
+          this.bookList.add(
+            "Harry Potter and the Order of the Phoenix",
+            "J. K. Rowling",
+            80,
+            null,
+            `Harry Potter and the Order of the Phoenix is a fantasy novel written by British author J. K. Rowling and the fifth novel in the Harry Potter series. It follows Harry Potter's struggles through his fifth year at Hogwarts School of Witchcraft and Wizardry, including the surreptitious return of the antagonist Lord Voldemort, O.W.L. exams, and an obstructive Ministry of Magic. The novel was published on 21 June 2003 by Bloomsbury in the United Kingdom, Scholastic in the United States, and Raincoast in Canada. It sold five million copies in the first 24 hours of publication`,
+            "images/81a4yXpXjnL._AC_UF1000,1000_QL80_.jpg"
+          );
+          this.bookList.add(
+            "Harry Potter and the Half-Blood Prince",
+            "J. K. Rowling",
+            80,
+            120,
+            `Harry Potter and the Half-Blood Prince is a fantasy novel written by British author J. K. Rowling and the sixth and penultimate novel in the Harry Potter series. Set during Harry Potter's sixth year at Hogwarts, the novel explores the past of the boy wizard's nemesis, Lord Voldemort, and Harry's preparations for the final battle against Voldemort alongside his headmaster and mentor Albus Dumbledore.
+            The book was published in the United Kingdom by Bloomsbury and in the United States by Scholastic on 16 July 2005, as well as in several other countries. It sold almost seven million copies in the first 24 hours after its release,[1] a record eventually broken by its sequel, Harry Potter and the Deathly Hallows.[2] There were many controversies before and after it was published, including the right-to-read copies delivered before the release date in Canada. Reception to the novel was generally positive, and it won several awards and honours, including the 2006 British Book of the Year award.`,
+            "images/81p2+4nYtkL._AC_UF894,1000_QL80_.jpg"
+          );
+          this.bookList.add(
+            "Harry Potter and the Deathly Hallows",
+            "J. K. Rowling",
+            80,
+            null,
+            `Harry Potter and the Deathly Hallows is a fantasy novel written by British author J. K. Rowling and the seventh and final novel in the Harry Potter series. It was released on 21 July 2007 in the United Kingdom by Bloomsbury Publishing, in the United States by Scholastic, and in Canada by Raincoast Books. The novel chronicles the events directly following Harry Potter and the Half-Blood Prince (2005) and the final confrontation between the wizards Harry Potter and Lord Voldemort.
+        
+            Deathly Hallows shattered sales records upon release, surpassing marks set by previous titles of the Harry Potter series. It holds the Guinness World Record for most novels sold within 24 hours of release, with 8.3 million sold in the US and 2.65 million in the UK.[1][2] Reception to the book was generally positive, and the book won the 2008 Colorado Blue Spruce Book Award, and the American Library Association named it the "Best Book for Young Adults". A play, Harry Potter and the Cursed Child, was written by Jack Thorne, and based on an original story written by J. K. Rowling, John Tiffany, and Thorne, premiering at the Palace Theatre in London on 30 July 2016. Rowling has referred to the play as "the eighth Harry Potter story", and is set nineteen years after the events of the Deathly Hallows, effectively picking up where the epilogue of the seventh book ended.`,
+            "images/81Whkax7IGL._AC_UF894,1000_QL80_.jpg"
+          );
+    }
+}
+
+
+const app = new App();
