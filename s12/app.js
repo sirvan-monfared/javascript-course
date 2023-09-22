f031936d3dce50285278bb0606ed135639793fd2
sirvan monfared <sirvan.monfared.dev@gmail.com>
2023-09-23 02:25:30 +0330

    s12e04

--- s12/app.js
+++ s12/app.js
@@ -3,20 +3,18 @@ const setBtn = document.getElementById('set');
 const getBtn = document.getElementById('get');
 
 setBtn.addEventListener('click', () => {
-    const user = {
-        name: 'sirvan',
-        lastName: ' monfared'
-    };
-
-    localStorage.setItem('uuid', JSON.stringify(user));
+    const userId = 34253253;
+    const user = {name: 'sirvan', age: 25};
+    document.cookie = `uuid=${userId}`;
+    document.cookie = `user=${JSON.stringify(user)};max-age=3`;
 })
 
 getBtn.addEventListener('click', () => {
-    const uuid = localStorage.getItem('uuid');
-    
-    if (uuid) {
-        console.log(JSON.parse(uuid));
-    } else {
-        console.log('sorry not exists');
-    }
+    const cookieData = document.cookie.split(";");
+    const data = cookieData.map(i => {
+        return i.trim();
+    }) 
+
+    const index = data.findIndex(item => item.includes('user='));
+    console.log(JSON.parse(data[index].split("=")[1]));
 })
